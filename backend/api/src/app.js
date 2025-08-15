f13e711e62c16d8e2f60fdcb7d59ec6ff6c1d877
Grxson <gael.grxson@gmail.com>
2025-08-15 10:45:25 -0600

    inicio del backend

---
+++ backend/api/src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const routes = require('./routes');
+
+const app = express();
+
+// Middlewares globales
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+// Rutas
+app.use('/api', routes);
+
+module.exports = app;
